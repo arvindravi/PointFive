671c44212a510dc1f40e657d8645ddb3ce6ef3b2
Arvind Ravi <arvind.ravi@icloud.com>
2021-05-03 11:18:25 +0100

    Initial Commit

---
+++ pointfive/pointfiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  pointfiveApp.swift
+//  pointfive
+//
+//  Created by Arvind Ravi on 03/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct pointfiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
